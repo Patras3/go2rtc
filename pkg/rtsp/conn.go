2e7c6813f96418ef86b5daa8fc156830776274c5
Patryk Dadas <patrykdadas@gmail.com>
2024-03-24 15:08:15 +0100

    Test with 100sec timeout

--- pkg/rtsp/conn.go
+++ pkg/rtsp/conn.go
@@ -128,7 +128,7 @@ func (c *Conn) Handle() (err error) {
 
 	case core.ModePassiveConsumer:
 		// pushing frames to remote RTSP Client (ex VLC)
-		timeout = time.Second * 86400
+		timeout = time.Second * 100
 
 	default:
 		return fmt.Errorf("wrong RTSP conn mode: %d", c.mode)
@@ -339,7 +339,7 @@ func (c *Conn) WriteResponse(res *tcp.Response) error {
 
 	if c.session != "" {
 		if res.Request != nil && res.Request.Method == MethodSetup {
-			res.Header.Set("Session", c.session+";timeout=86400")
+			res.Header.Set("Session", c.session+";timeout=100")
 		} else {
 			res.Header.Set("Session", c.session)
 		}
